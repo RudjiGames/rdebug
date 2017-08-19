586de3f64a7285ebc158eeff5695c27e062e837c
Milos Tosic <milostosic77@gmail.com>
2017-08-19 10:02:14 +0800

    Delete file

--- src/rdebug.cpp
+++
@@ -1,7 +0,0 @@
-//--------------------------------------------------------------------------//
-/// Copyright (c) 2017 by Milos Tosic. All Rights Reserved.                /// 
-/// License: http://www.opensource.org/licenses/BSD-2-Clause               ///
-//--------------------------------------------------------------------------//
-
-#include <rdebug_pch.h>
-
