dbed042cccad8c30e6bc7d282b03534d6cf71414
Milos Tosic <milostosic77@gmail.com>
2018-07-25 09:06:14 +0800

    Update build script

--- genie/rdebug.lua
+++ genie/rdebug.lua
@@ -3,27 +3,27 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
-function projectLink_DIA()
+function projectDependencies_rdebug()
+	return { "rbase" }
+end
+
+function projectExtraConfigExecutable_rdebug()
 	configuration {"windows", "x32", "not gmake" }
+		includedirs { getProjectPath("DIA", ProjectPath.Root) }
 		libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x32/" }
 		links {"diaguids"}
 	configuration {"windows", "x64", "not gmake" }
+		includedirs { getProjectPath("DIA", ProjectPath.Root) }
 		libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x64/" }
 		links {"diaguids"}
-	configuration {}		
+	configuration {}
 end
-
-function projectDependencies_rdebug()
-	return { "DIA", "rbase" }
-end
-
 
 function projectExtraConfig_rdebug()
 	configuration { "gmake" }
 		if "mingw-gcc" == _OPTIONS["gcc"] then -- on windows, we patch heap functions, no need to wrap malloc family of funcs
 			buildoptions { "-Wno-unknown-pragmas" }
 		end
-	configuration {}
 end
 
 function projectAdd_rdebug()
