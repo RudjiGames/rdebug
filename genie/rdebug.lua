66fc48e3bbf607fbfc2ca044d2c4250f367e3f9b
Milos Tosic <milostosic77@gmail.com>
2023-09-12 15:14:17 +0200

    Add target OS check before adding DIA

--- genie/rdebug.lua
+++ genie/rdebug.lua
@@ -8,15 +8,17 @@ function projectDependencies_rdebug()
 end
 
 function projectExtraConfigExecutable_rdebug()
-	configuration {"windows", "x32", "not gmake" }
-		includedirs { getProjectPath("DIA", ProjectPath.Root) }
-		libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x32/" }
-		links {"diaguids"}
-	configuration {"windows", "x64", "not gmake" }
-		includedirs { getProjectPath("DIA", ProjectPath.Root) }
-		libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x64/" }
-		links {"diaguids"}
-	configuration {}
+	if getTargetOS() == "windows" then
+		configuration {"windows", "x32", "not gmake" }
+			includedirs { getProjectPath("DIA", ProjectPath.Root) }
+			libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x32/" }
+			links {"diaguids"}
+		configuration {"windows", "x64", "not gmake" }
+			includedirs { getProjectPath("DIA", ProjectPath.Root) }
+			libdirs { getProjectPath("DIA", ProjectPath.Root) .. "DIA/lib/x64/" }
+			links {"diaguids"}
+		configuration {}
+	end
 end
 
 function projectExtraConfig_rdebug()
