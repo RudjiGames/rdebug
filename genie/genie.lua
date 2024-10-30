47e9e3b156f59dbd0c7d0878d8298c0b8b6c7f2f
Милош Тошић <1654399+milostosic@users.noreply.github.com>
2024-10-30 20:46:19 +0100

    Update genie.lua

--- genie/genie.lua
+++ genie/genie.lua
@@ -9,7 +9,6 @@ if os.isfile(tp) then dofile (tp) break end end if RTM_ROOT_DIR == nil then
 print("EROR: dependency missing - build. Cannot continue.") os.exit() end
 
 solution "rdebug"
-	configurations { "debug", "release", "retail" }
 	setPlatforms()
 
 	loadProject("rdebug")
